f83c608a4e0a45b231897b11c008a5ba25302282
tandasat <tanda.sat@gmail.com>
2015-06-01 21:55:54 -0700

    Remove compiled files

--- Scavenger/clean.bat
+++ Scavenger/clean.bat
@@ -1,11 +1,15 @@
 del *.sdf
 del /a:h *.suo
 rmdir /s /q ipch
-rmdir /s /q Debug
-rmdir /s /q Release
+rmdir /s /q Win7Debug
+rmdir /s /q Win7Release
+rmdir /s /q Win8.1Debug
+rmdir /s /q Win8.1Release
 rmdir /s /q x64
-rmdir /s /q Scavenger\Debug
-rmdir /s /q Scavenger\Release
+rmdir /s /q Scavenger\Win7Debug
+rmdir /s /q Scavenger\Win7Release
+rmdir /s /q Scavenger\Win8.1Debug
+rmdir /s /q Scavenger\Win8.1Release
 rmdir /s /q Scavenger\x64
 del /s *.aps
 pause
